e749bafb0689516dfa3128f2e1c797ad5a26df08
isanawaz <isanawaz27@gmail.com>
2025-03-17 09:55:04 -0400

    Create Project.java

---
+++ CSCI-211/Project.java
@@ -0,0 +1,19 @@
+/**
+ * Main class for the Appliance Management Application.
+ * This class is responsible for initializing the graphical user interface (GUI) to manage appliances.
+ * 
+ * The application provides functionality to load appliance data from a file, validate it, 
+ * categorize appliances by type, and display them within the GUI.
+ */ 
+public class Project4 {
+	/**
+     * The main method that starts the application by creating an instance of the {@link ApplianceGUI}.
+     * 
+     * @param args command-line arguments (not utilized in this application)
+     */
+	public static void main(String[] args) { 
+		 // Initialize and show the appliance management GUI
+		new ApplianceGUI(); 
+	}
+}
+
